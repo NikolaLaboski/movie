7a6ff2dca949b7c8c570cd13d628e596fff662ec
Nikola Laboski <nikola.laboski@outlook.com>
2021-11-26 00:30:29 +0100

    deploy

---
+++ build/precache-manifest.75c774001b3f9b2c9f4261a92de510d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a111c1027b98764607881ad5409d6da4",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "ceb930b0398fc9ba17f1",
+    "url": "/movie/static/css/2.140c15d5.chunk.css"
+  },
+  {
+    "revision": "127a282f33dba133651d",
+    "url": "/movie/static/css/main.14ec5843.chunk.css"
+  },
+  {
+    "revision": "ceb930b0398fc9ba17f1",
+    "url": "/movie/static/js/2.78ce5578.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie/static/js/2.78ce5578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127a282f33dba133651d",
+    "url": "/movie/static/js/main.4e9857d6.chunk.js"
+  },
+  {
+    "revision": "6b2e52c656c4cc4688ee",
+    "url": "/movie/static/js/runtime-main.103c8af6.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/movie/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/movie/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/movie/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/movie/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  }
+]);
